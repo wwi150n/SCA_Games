fb33f1d5455e8eb21b85c5cbec9ca900fdba8f55
Sylvia Dewi <Slvi7d@gmail.com>
2021-01-02 04:04:00 +0700

    Fitur_XOXO

--- Hames/Menu_Utama/XOXO_Menu.cs
+++ Hames/Menu_Utama/XOXO_Menu.cs
@@ -12,9 +12,125 @@ namespace Menu_Utama
 {
     public partial class XOXO_Menu : Form
     {
+        bool turn = true; // true = x trun, false O turn
+        int turn_count = 0;
+
         public XOXO_Menu()
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("XOXO By Sylvia Dewi Chaiyanti", "XOXO about");
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)
+                b.Text = "X";
+            else
+                b.Text = "O";
+
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+
+            checkForWinner();
+
+        }
+        private void checkForWinner()
+        {
+            bool there_is_a_winner = false;
+
+            // Horizontal
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                there_is_a_winner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+
+
+            // Vertikal
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                there_is_a_winner = true;
+
+            // Diagonal
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                there_is_a_winner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+                there_is_a_winner = true;
+
+            if (there_is_a_winner)
+            {
+                disableButtons();
+               string winner = "";
+                if (turn)
+                    winner = "O";
+                else
+                    winner = "X";
+                MessageBox.Show(winner + "WIN!", "CONGRATS");
+            }//end if
+            else
+            {
+                if (turn_count == 9)
+                    MessageBox.Show("DRAW!", "UPS");
+            }
+        }// end check for winner
+
+        private void disableButtons ()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }//end foreach
+            } // end try
+            catch{ }
+        
+        }
+     
+
+        private void click(object sender, EventArgs e)
+        {
+            
+                turn = true;
+                turn_count = 0;
+                try
+                {
+                    foreach (Control c in Controls)
+                    {
+                        Button b = (Button)c;
+                        b.Enabled = true;
+                        b.Text = "";
+                    }//end foreach
+                } // end try
+                catch { }
+
+            }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
-}
+    }
+
