fe10e5e91493ef12f8d2d885409492ebeef553ef
Wilson <wilm3son@gmail.com>
2021-01-06 19:21:05 +0700

    BINGO - Update Fitur Isi Nama

--- Hames/Menu_Utama/BINGO_IsiNama.Designer.cs
+++ Hames/Menu_Utama/BINGO_IsiNama.Designer.cs
@@ -109,6 +109,7 @@
             this.button1.TabIndex = 5;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // BINGO_IsiNama
             // 
--- Hames/Menu_Utama/BINGO_IsiNama.cs
+++ Hames/Menu_Utama/BINGO_IsiNama.cs
@@ -19,7 +19,9 @@ namespace Menu_Utama
             InitializeComponent();
             string x;
             x = File.ReadAllText("Nilai.txt");
-
+            nilai = Convert.ToDouble(x);
+            File.Delete("Nilai.txt");
+            label3.Text = nilai.ToString();
         }
 
         private void BINGO_IsiNama_Load(object sender, EventArgs e)
@@ -30,6 +32,46 @@ namespace Menu_Utama
         private void BINGO_IsiNama_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool valid = true;
+            string nama;
+            nama = textBox1.Text;
+            if (textBox1.Text.Length==0)
+            {
+                valid = false;
+            }
+            else
+            {
+                if (nama[0] ==' ')
+                {
+                    int index = 0;
+                    valid = false;
+                    for (int i = 0; i < nama.Length; i++)
+                    {
+                        if(nama[i]!=' ')
+                        {
+                            valid = true;
+                            index = i;
+                            break;
+                        }
+                    }
+                    string temp;
+                    temp = nama;
+                    for (int j = 0; j < index; j++)
+                    {
+                        temp.Remove(0, 1);
+                    }
+                    nama = temp;
+                }
+            }
+            if (!valid) MessageBox.Show("Nama tidak boleh kosong !!!");
+            else
+            {
+                //menu daftar score
+            }
         }
     }
 }
