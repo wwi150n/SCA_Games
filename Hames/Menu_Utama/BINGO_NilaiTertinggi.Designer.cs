27722cf06b8e676df857ea4800b7f21d8ad30742
Wilson <wilm3son@gmail.com>
2021-01-08 23:56:05 +0700

    BINGO - Update Fitur Nilai Tertinggi

--- Hames/Menu_Utama/BINGO_NilaiTertinggi.Designer.cs
+++ Hames/Menu_Utama/BINGO_NilaiTertinggi.Designer.cs
@@ -29,31 +29,22 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(219, 20);
+            this.label1.Location = new System.Drawing.Point(89, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(96, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "NILAI TERTINGGI";
             // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Enabled = false;
-            this.richTextBox1.Location = new System.Drawing.Point(12, 50);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(498, 96);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(222, 227);
+            this.button1.Location = new System.Drawing.Point(110, 227);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -61,13 +52,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(9, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(263, 173);
+            this.listBox1.TabIndex = 3;
+            // 
             // BINGO_NilaiTertinggi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 262);
+            this.ClientSize = new System.Drawing.Size(283, 262);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label1);
             this.Name = "BINGO_NilaiTertinggi";
             this.Text = "BINGO_NilaiTertinggi";
@@ -81,7 +80,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
--- Hames/Menu_Utama/BINGO_NilaiTertinggi.cs
+++ Hames/Menu_Utama/BINGO_NilaiTertinggi.cs
@@ -23,6 +23,12 @@ namespace Menu_Utama
             Application.Exit();
         }
 
+        public struct Data
+        {
+            public float n;
+            public string nama;
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             BINGO_Menu frm = new BINGO_Menu();
@@ -32,22 +38,82 @@ namespace Menu_Utama
 
         private void BINGO_NilaiTertinggi_Load(object sender, EventArgs e)
         {
-            string n="";
-            int hitung = 1;
-            string a;
-            string[] b;
+            listBox1.Items.Clear();
+            Data temp = new Data();
+            string[] y1;
+            List<Data> daftar = new List<Data>();
+            temp.n = 59f;
+            temp.nama = "Agus";
+            daftar.Add(temp);
+            temp.n = 58f;
+            temp.nama = "Budi";
+            daftar.Add(temp);
+            temp.n = 57f;
+            temp.nama = "Chitra";
+            daftar.Add(temp);
+            temp.n = 56f;
+            temp.nama = "Domino";
+            daftar.Add(temp);
+            temp.n = 55f;
+            temp.nama = "Erlangga";
+            daftar.Add(temp);
+            temp.n = 54f;
+            temp.nama = "Fajar";
+            daftar.Add(temp);
+            temp.n = 53f;
+            temp.nama = "Gandhi";
+            daftar.Add(temp);
+            temp.n = 52f;
+            temp.nama = "Hansono";
+            daftar.Add(temp);
+            temp.n = 51f;
+            temp.nama = "Ijun";
+            daftar.Add(temp);
+            temp.n = 50f;
+            temp.nama = "Jaguar";
+            daftar.Add(temp);
             if (File.Exists("Nama.txt"))
             {
-                StreamReader sr = new StreamReader("Nama.txt");
-                do
+                y1 = File.ReadAllLines("Nama.txt");
+                for (int i = 0; i < y1.Length; i++)
                 {
-                    a = sr.ReadLine();
-                    b = a.Split();
-                    n += hitung.ToString()+"\t"+b[0]+"\t"+b[1];
-                    hitung++;
-                } while (!sr.EndOfStream);
-                sr.Close();
-                richTextBox1.Text = n;
+                    string[] temp2 = y1[i].Split();
+                    temp.nama = temp2[0];
+                    temp.n = Convert.ToSingle(temp2[1]);
+                    daftar.Add(temp);
+                }
+            }
+
+            for (int i = 0; i < daftar.Count - 1; i++)
+            {
+                for (int j = i + 1; j < daftar.Count; j++)
+                {
+                    if (daftar[i].n < daftar[j].n)
+                    {
+                        temp = daftar[i];
+                        daftar[i] = daftar[j];
+                        daftar[j] = temp;
+                    }
+                }
+            }
+            File.Delete("Nama.txt");
+            File.Create("Nama.txt").Close();
+            for (int i = 0; i < 10; i++)
+            {
+                File.AppendAllText("Nama.txt", daftar[i].nama + " " + daftar[i].n + "\n");
+            }
+            string[] y = File.ReadAllLines("Nama.txt");
+            for (int i = 0; i < y.Length; i++)
+            {
+                string a;
+                string[] b;
+                a = y[i].ToString();
+                b = a.Split();
+                Data x = new Data();
+                x.nama = b[0].ToString();
+                x.n = Convert.ToSingle(b[1]);
+                string hasil = $"{(i + 1).ToString().PadLeft(2,'0')}.\t{x.nama.PadRight(20)}\t{x.n}";
+                listBox1.Items.Add(hasil);
             }
 
         }
